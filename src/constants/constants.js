f544e4895ec78bd745bb211a6cd6c88a60d20684
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-26 13:48:17 +0400

    Edited main btns in hero section

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -39,13 +39,6 @@ export const projectsList = [
   },
 ];
 
-export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my journey', },
-];
 
 
 export const features = [
