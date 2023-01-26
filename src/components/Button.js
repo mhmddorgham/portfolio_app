4e925df6a4155245d9813554c4137431e8b535dc
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-26 15:44:14 +0400

    Edited The website content

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button type='button' className='py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10 rounded-xl ' >{props.value}</button>
+    <button type='button' className='py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10 rounded-xl' >{props.value}</button>
   )
 }
 
