4a993285e467064e2fb93f2ab9eee3ba97bf314e
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-28 00:03:08 +0400

    Edited the card section

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,11 @@ const Card = (props) => {
   return (
 
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
+      <a href={props.visitLink}>
         <img class="rounded-t-lg" src={props.img} alt="" />
       </a>
       <div class="p-5">
-        <a href="#">
+        <a href={props.visitLink}>
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.title}</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 py-5 ">{props.description}</p>
