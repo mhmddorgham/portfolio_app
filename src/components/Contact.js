68741476419ef16cc7e1436964f965af5e4f39b9
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-28 15:46:06 +0400

    Add social links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,6 +24,7 @@ const Contact = () => {
   })()
 
 
+
   return (
 
     <div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { socialMedia } from '../constants/constants'
+import { AiFillTwitterCircle, AiFillFacebook, AiFillInstagram, AiFillLinkedin } from "react-icons/ai"
 
 const Footer = () => {
   const currYear = new Date().getFullYear()
@@ -7,15 +7,11 @@ const Footer = () => {
     <footer className='py-5'>
       <div className=' w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-white'>
         <p className='font-normal font-poppins text-[18px] leading-[27px] text-white text-center'> {currYear} &copy; Mohammed Dorgham </p>
-        <div className='flex flex-row md:mt-0 mt-6'>
-          {socialMedia.map((social, i) => (
-            <img
-              key={social.id}
-              src={social.icon}
-              alt={social.id}
-              className={`w-[21px] h-[21px] object-contain cursor-pointer ${i !== socialMedia.length ? "mr-6" : "mr-0"}`}
-            />
-          ))}
+        <div className='flex flex-row md:mt-0 mt-6 gap-4'>
+          <AiFillFacebook className='text-white text-[25px]' />
+          <AiFillTwitterCircle className='text-white text-[25px]' />
+          <AiFillInstagram className='text-white text-[25px]' />
+          <AiFillLinkedin className='text-white text-[25px]' />
         </div>
       </div>
     </footer>
